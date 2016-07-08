32c0f7bb560d08555726ac7ca10865bf9ff73b89
timi <timi.novotny@mac.com>
2016-07-08 11:45:45 +0200

    code changes

--- Nussalergie/NeueRezeptViewController.swift
+++ Nussalergie/NeueRezeptViewController.swift
@@ -6,23 +6,30 @@
 //  Copyright © 2016 Scheissegal. All rights reserved.
 //
 
-import Cocoa
+import UIKit
+import Firebase
 
-class NeueRezeptViewController: NSViewController {
+class NeueRezeptViewController: UIViewController {
     
     
-    let rezeptImageView: UIImageView = UIImageView()
-    let rezeptNameLabel: UILabel = UILabel()
-    let rezeptDauerLabel: UILabel = UILabel()
-    let rezeptZubereitungLabel: UILabel = UILabel()
-    let rezeptNameTextfield: UITextfield
     let rezeptNavigationTitle: String = "Neues Rezept"
-    let rezeptDescriptionLabel: UILabel = UILabel()
+    let rezeptImageView: UIImageView = UIImageView()
+    let ImageViewwidth: CGFloat = 150;
+    let rezeptNameLabel: UILabel = UILabel()
+    let rezeptNameTextfield: UITextField = UITextField()
+    let rezeptDauerLabel: UILabel = UILabel()
+    let rezeptDauerTextfield: UITextField = UITextField()
+    let rezeptZubereitungLabel: UILabel = UILabel()
+    let rezeptZubereitungstext: UITextField = UITextField()
+    let ZubereitungsTextfieldheight: CGFloat = 200;
+    let rezeptZutatenLabel:UILabel = UILabel()
+    let rezeptZutatenButton: UIButton = UIButton()
+    let rezeptzutatenbuttonLabel: UILabel = UILabel()
+    
     var rezeptID: Int = Int()
     
     let scrollView: UIScrollView = UIScrollView()
-    let zutatenTableView: UITableView = UITableView()
-    
+   
     
     
     
@@ -33,6 +40,45 @@ class NeueRezeptViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+         self.navigationItem.title = "Neues Rezept"
+        scrollView.frame = CGRectMake(0, 0, view.frame.width, view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width, height: 2000)
+        
+        
+        rezeptImageView.frame = CGRectMake(view.frame.width - ImageViewwidth - 20, 20 ,ImageViewwidth,ImageViewwidth)
+        rezeptImageView.backgroundColor = UIColor.blackColor()
+        
+        rezeptNameLabel.frame = CGRectMake(20, 20, 100, 30)
+        rezeptNameLabel.text = "Name"
+        rezeptNameTextfield.frame = CGRectMake(20, 55, 100, 30)
+        rezeptDauerLabel.frame = CGRectMake(20, 100, 100, 30)
+        rezeptDauerLabel.text = "Dauer"
+        rezeptDauerTextfield.frame = CGRectMake(20, 135, 100, 30)
+        rezeptZubereitungLabel.frame = CGRectMake(20, 180, 100, 30)
+        rezeptZubereitungLabel.text = "Zubereitung"
+        rezeptZubereitungstext.frame = CGRectMake(20, 205,view.frame.width - 40 , ZubereitungsTextfieldheight)
+        rezeptZutatenLabel.frame = CGRectMake(20, 210 + ZubereitungsTextfieldheight + 10, 100, 30)
+        rezeptZutatenLabel.text = "Zutaten"
+        rezeptZutatenButton.frame = CGRectMake(view.frame.width - 120, 210 + ZubereitungsTextfieldheight + 10, 100, 30)
+        rezeptzutatenbuttonLabel.frame = CGRectMake(view.frame.width - 120, 210 + ZubereitungsTextfieldheight + 10, 100, 30)
+        rezeptzutatenbuttonLabel.text = "neue Zutat"
+        
+        
+        scrollView.addSubview(rezeptImageView)
+        scrollView.addSubview(rezeptNameLabel)
+        scrollView.addSubview(rezeptNameTextfield)
+        scrollView.addSubview(rezeptDauerLabel)
+        scrollView.addSubview(rezeptDauerTextfield)
+        scrollView.addSubview(rezeptZubereitungLabel)
+        scrollView.addSubview(rezeptZubereitungstext)
+        scrollView.addSubview(rezeptZutatenLabel)
+        scrollView.addSubview(rezeptZutatenButton)
+        scrollView.addSubview(rezeptzutatenbuttonLabel)
+        
+        
+        
+        view.addSubview(scrollView)
     }
     
 }
